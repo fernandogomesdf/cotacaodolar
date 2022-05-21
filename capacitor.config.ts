5d86baafe34ef23101ca68d032d04b051697ac70
Fernando Gomes <ffernando_gomes@hotmail.com>
2022-05-21 12:32:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cotação dolar',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
